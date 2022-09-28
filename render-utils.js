14afd9f4d59195ac044b86a6c179e6965886667e
Marty Nelson <marty@martypdx.com>
2022-09-28 11:21:36 -0700

    better dead cat display

--- render-utils.js
+++ render-utils.js
@@ -1,6 +1,10 @@
 export function renderCat(cat) {
     const li = document.createElement('li');
 
+    if (cat.lives < 1) {
+        li.classList.add('dead');
+    }
+
     const p = document.createElement('p');
     p.textContent = `${cat.name} (${cat.lives} lives)`;
 
